4cc8824bf5d32bda575abb7f0d231215934fa578
Josue(MSFT) <v-zhli15@microsoft.com>
2020-10-09 10:37:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for
  * license information.
+ * 
+ * This file uses an outdated library.  Please see the readme to find the latest version.
  */
 'use strict';
 var http = require('http');
